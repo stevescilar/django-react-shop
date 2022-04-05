afd70fa4985642474402faf5b307ef06f3e9092f
stevescilar <stevemuambi@gmail.com>
2022-04-05 12:43:04 +0300

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <div>
-      my app
+      <Header />
+      <main>
+        <h1> Welcome Pro-shopper </h1>
+      </main>
+      <Footer/>
     </div>
   );
 }
