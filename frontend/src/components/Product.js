e8fbb7e753f13f9085eb15212dc6d35111e8c305
stevescilar <stevemuambi@gmail.com>
2022-04-05 14:27:10 +0300

    updated

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -8,6 +8,21 @@ function Product( { product } ) {
         <a href={`/product/${product._id}`}>
             <Card.Img src={product.image} />
         </a>
+        <Card.Body>
+            <a href={`/product/${product._id}`}>
+                <Card.Title as="div">
+                    <strong>
+                        {product.name}
+                    </strong>
+                </Card.Title>
+            </a>
+            <Card.Text as="div">
+                <div className='my-3'>
+                    {product.rating} from {product.numReviews} Reviews
+                </div>
+
+            </Card.Text>
+        </Card.Body>
     </Card>
   )
 }
