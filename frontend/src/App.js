9b9a98a38037f1517172d408bc23e8f916b05a64
stevescilar <stevemuambi@gmail.com>
2022-04-07 16:06:44 +0300

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -10,9 +10,9 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <Routes>
-          <Route path = '/' component={HomeScreen} exact /> 
-          </Routes>
+          
+          <Route path ='/' component={HomeScreen} exact /> 
+        
         </Container>
       </main>
       <Footer/>
